29b53023fd584ff61b0179274d9fc8faaa15feda
yeshaa23 <ayeshanazkiya@gmail.com>
2026-07-06 16:36:23 +0700

    Use persistent public storage path on Azure

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,11 +40,10 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'root' => env('PUBLIC_DISK_ROOT', storage_path('app/public')),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
